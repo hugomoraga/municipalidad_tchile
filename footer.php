ad79ab68885d5e3639be338fe4d5e1390fcfc288
valeria.carrillo <valeriacarodr@gmail.com>
2021-12-21 11:24:42 -0300

    EDIT: footer

--- footer.php
+++ footer.php
@@ -10,22 +10,66 @@
  */
 
 ?>
-
+	<style>
+		.site-info {
+			padding: 15px;
+			background-color: #0082B6;
+			font-weight: 700;
+			color: white;
+		}
+		.site-info h5 {
+			margin-top: 10px;
+			color: #E5BB0C
+		}
+	</style>
+	
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'municipalidad_tchile' ) ); ?>">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm">
+						MESA CENTRAL: 33 2381700<br><br>
+						DIDECO:<br>
+						Av.Gonzalo Lizasoaín 405.<br>
+						Fono: 33 2381811<br><br>
+
+						ÁREA OPERATIVA:<br>
+						Secretaría: 33 2227477<br>
+						Movilización: 228-853093<br>
+						Portería: 228-853901<br><br>
+						OMIL: 332381828<br><br>
+						OBRAS: 33 2381712<br><br>
+						TRÁNSITO: 33 2381705<br><br>
+						SECPLA: 33 2381746<br><br>
+						SEGURIDAD PÚBLICA: 223249074<br><br>
+						DEPORTES: 9 61225542<br><br>
+						MEDIO AMBIENTE: 224742469<br><br>
+						CEMENTERIO: 33 2262394<br><br>
+						FARMACIA COMUNAL: 33 2211019
+					</div>
+					<div class="col-sm">
+					One of three columns
+					</div>
+					<div class="col-sm">
+					One of three columns
+					</div>
+				</div>
+			</div>
+			<h5>I. Municipalidad de La Calera 
+				/MESA CENTRAL Fono: 33 2381700 DERECHOS RESERVADOS
+			</h5>
+			<!--<a href="<?php //echo esc_url( __( 'https://wordpress.org/', 'municipalidad_tchile' ) ); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'municipalidad_tchile' ), 'WordPress' );
+				//printf( esc_html__( 'Proudly powered by %s', 'municipalidad_tchile' ), 'WordPress' );
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'municipalidad_tchile' ), 'municipalidad_tchile', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			</a> -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+
+
+
+
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
